915edac3474775b59188bb83f658cfa6b498115b
homosapian <evolvingsince2000@gmail.com>
2025-12-04 15:19:10 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Navbar from '#components/Navbar'
+
+function App() {
+
+
+  return (
+    <>
+    <main>
+      <Navbar />
+    </main>
+    </>
+  )
+}
+
+export default App
